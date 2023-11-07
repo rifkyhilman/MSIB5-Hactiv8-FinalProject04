98672394002391a9cc3214597971f73d62f1dba7
rifkyhilman <rifkyhilman265@gmail.com>
2023-11-07 07:04:42 +0700

    :construction: Component Weather
    Component Weather

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,57 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+      <div class="header container h-100 p-5">
+        <h1 class="mb-5">Weather App</h1>
+        <div class="d-flex justify-content-center h-100">
+          <div class="searchbar w-50 mx-2">
+            <input type="text" class="input form-control" v-model="city" placeholder="Enter a City">
+          </div>
+          <button class="btn-search btn btn-primary"><i class="fas fa-search"></i></button>
+        </div>
+      </div>
+  </div>
+  <br>
+  <Weather />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Weather from './components/Weather.vue';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Weather
+  },
+  data () {
+    return {
+      city: '',
+      showWeather: false,
+    }
+  },
+  methods: {
+    async searchWeather() {
+      this.showWeather = false;
+      await this.$nextTick();
+      this.showWeather = true;
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+body {
+  background-color: #121212 !important;
 }
+
+.header {
+  background-color: #212730;
+  border-radius: 20px;
+  color: #fff;
+  text-align: center;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  margin-top: 5rem;
+}
+
 </style>
---
+++ src/components/Weather.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="container p-0">
+        <div class="d-flex">
+            <div class="card main-div w-100">
+                <div class="p-3">
+                    <h2 class="mb-1 day">Tuesday</h2>
+                    <p class="text-light date mb-0">date</p>
+                    <small>time</small>
+                    <h2 class="place"><i class="fa fa-location">Rio <small>country</small></i></h2>
+                    <div class="temp">
+                        <h1 class="weather-temp">19&deg;</h1>
+                        <h2 class="text-light">description</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card card-2 w-100">
+            <table class="m-4">
+                <tbody>
+                    <tr>
+                        <th>Seal</th>
+                        <td>100</td>
+                    </tr>
+                    <tr>
+                        <th>Seal</th>
+                        <td>100</td>
+                    </tr>
+                    <tr>
+                        <th>Seal</th>
+                        <td>100</td>
+                    </tr>
+                    <tr>
+                        <th>Seal</th>
+                        <td>100</td>
+                    </tr>
+                </tbody>
+            </table>
+            <div id="div_Form" class="d-flex m-3 justify-content-center">
+                <form action="">
+                    <input type="button" value="change location" class="btn change-btn btn-primary">
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'myWaether',
+    }
+</script>
+
+<style>
+
+body {
+    background-color: #343d4b;
+}
+
+.weather-temp {
+    margin: 0;
+    font-weight: 700;
+    font-size: 4rem;
+}
+
+</style>
